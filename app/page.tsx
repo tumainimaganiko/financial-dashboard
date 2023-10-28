0a699b38b9a6ef37b60b7bba2dcdb4af4511a50c
tumainimaganiko <tumainimaganiko@gmail.com>
2023-10-28 13:19:09 +0300

    Add Image Asset

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { lusitana } from '@/app/ui/fonts';
 import Link from 'next/link';
+import Image from 'next/image';
+
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
@@ -26,6 +28,8 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image src="/hero-desktop.png" width={1000} height={760} className='hidden md:block' alt='Screenshots of the dashboard project showing desktop and mobile versions' />
+          <Image src="/hero-mobile.png" width={560} height={620} className='md:hidden' alt='Screenshots of the dashboard project showing desktop and mobile versions' />
         </div>
       </div>
     </main>
